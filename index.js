6a1ac97e202949ecb40614f43952bce13944e4bf
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-04-21 15:19:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = 5001
+const cors = require('cors')
+app.use(cors())
+const bodyParser = require('body-parser')
+
+app.use(express.json())
+
+const users = [
+    {id:1, name: 'Rayhan' , email: 'rayhan@gmail.com', phone: "01776464695"},
+    {id:2, name: 'Shamim' , email: 'Shamim@gmail.com', phone: "0172584562"},
+    {id:3, name: 'Rahat' , email: 'Rahat@gmail.com', phone: "016249456"},
+    {id:4, name: 'Tarekh' , email: 'Tarekh@gmail.com', phone: "01365954445"},
+    {id:5, name: 'iqbal' , email: 'iqbal@gmail.com', phone: "01658645654"},
+    {id:6, name: 'abdullah' , email: 'abdullah@gmail.com', phone: "01448484848"},
+    
+]
+
+app.post('/user',(req,res) => {
+    const userInfo = req.body
+    userInfo.id = users.length + 1
+    users.push(userInfo)
+    console.log(userInfo)
+    res.send(userInfo)
+})
+
+app.get('/users',(req,res) => {
+    const user = req.query
+    console.log(user)
+    if(user?.name){
+        const searchUser = users.filter(u => u.name.toLowerCase().includes(user?.name))
+        res.send(searchUser.length ? searchUser : "not found")
+    }else{
+        res.send(users)
+    }
+})
+
+
+app.listen(port,() => {
+    console.log('amar sonar bangla')
+})
